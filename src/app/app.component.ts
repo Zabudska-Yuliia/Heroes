28960b8bfbbc189a831aeaa7c871c7ea589d02ba
Zabudska-Yuliia <yuliiaz@softwareplanet.uk.com>
2016-11-30 17:39:21 +0200

    Create project to get started Angular
    - add ability to show hero
    - add template for show hero
    - add  ability to show heros list
    - add template for show heros list

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+const HEROES: Hero[] = [
+  { id: 1, name: 'Mr. Nice' },
+  { id: 2, name: 'Narco' },
+  { id: 3, name: 'Bombasto' },
+  { id: 4, name: 'Celeritas' },
+  { id: 5, name: 'Magneta' },
+  { id: 6, name: 'RubberMan' },
+  { id: 7, name: 'Dynama' },
+  { id: 8, name: 'Dr IQ' },
+  { id: 9, name: 'Magma' },
+  { id: 10, name: 'Tornado' }
+];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
+
+@Component({
+  selector: 'my-hero-detail',
+})
+export class HeroDetailComponent {
+}
