b8ba154c910b422e3e86668f23cba00cd752608f
Zabudska-Yuliia <yuliiaz@softwareplanetgroup.com>
2016-12-02 11:42:26 +0200

    Add ability to save, delete and change heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,7 +8,7 @@ import { Hero } from './hero';
 @Injectable()
 export class HeroService {
     private heroesUrl = '/api/hero/';
-
+    private headers = new Headers({'Content-Type': 'application/json'});
     constructor(private http: Http) { }
 
 
@@ -31,8 +31,34 @@ export class HeroService {
             .then(heroes => heroes.find(hero => hero.id === id));
     }
 
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url, JSON.stringify(hero), {headers: this.headers})
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
+    }
+
+    create(name: string): Promise<Hero> {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+            .toPromise()
+            .then(res => res.json())
+            .catch(this.handleError);
+    }
+
+    delete(id: number): Promise<void> {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.delete(url, {headers: this.headers})
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError);
+    }
+
     private handleError(error: any): Promise<any> {
         console.error('An error occurred', error); // for demo purposes only
         return Promise.reject(error.message || error);
     }
+
 }
